c57a077c298ae54dbdf615e3a19e0a91feb434af
Abdullah <150387908+Abdullahateeq00@users.noreply.github.com>
2024-04-17 20:42:00 -0700

    Q9
    Signed-off-by: Abdullah  <150387908+Abdullahateeq00@users.noreply.github.com>

---
+++ Q9/q9.js
@@ -0,0 +1,4 @@
+// Q9: Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favNumber = 10;
+var message = "Mine favourite Number is:";
+console.log(message, favNumber);
---
+++ Q9/q9.ts
@@ -0,0 +1,5 @@
+// Q9: Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favNumber = 10;
+let message = "Mine favourite Number is:";
+console.log(message, favNumber);
