735b3d72a7571039e6e961ee7c7011f0fdd06013
Abdullah <150387908+Abdullahateeq00@users.noreply.github.com>
2024-04-21 12:26:33 +0500

    Q11
    Signed-off-by: Abdullah  <150387908+Abdullahateeq00@users.noreply.github.com>

---
+++ Q11/q11.js
@@ -0,0 +1,8 @@
+"use strict";
+// Q11: Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+Object.defineProperty(exports, "__esModule", { value: true });
+let names = ["Abdullah", "Ateeq Ahmed", "Huzaifa", "Abdulrab"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
