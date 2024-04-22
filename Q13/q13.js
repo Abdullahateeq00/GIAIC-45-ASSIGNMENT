76a8d1dea7c1df38ffa9defd666d26cdc86d0287
Abdullah <150387908+Abdullahateeq00@users.noreply.github.com>
2024-04-22 05:14:26 +0500

    QUESTION 13
    Signed-off-by: Abdullah  <150387908+Abdullahateeq00@users.noreply.github.com>

---
+++ Q13/q13.js
@@ -0,0 +1,7 @@
+"use strict";
+// Q13:Arrays help you group related items, like types of transportation. This way, you can talk about each one using a loop.
+Object.defineProperty(exports, "__esModule", { value: true });
+let transport = ["fortuner", "thar", "ferrari"];
+transport.forEach(transport => {
+    console.log(`i would like to own this ${transport}`);
+});
---
+++ Q13/q13.ts
@@ -0,0 +1,8 @@
+// Q13:Arrays help you group related items, like types of transportation. This way, you can talk about each one using a loop.
+
+
+let transport: string [] = ["fortuner", "thar", "ferrari"];
+transport.forEach(transport => {
+    console.log(`i would like to own this ${transport}`);
+    
+});
