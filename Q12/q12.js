1b29cbe44f90444cc272217c877e24433af78bf7
Abdullah <150387908+Abdullahateeq00@users.noreply.github.com>
2024-04-22 05:12:52 +0500

    QUESTION 12
    Signed-off-by: Abdullah  <150387908+Abdullahateeq00@users.noreply.github.com>

---
+++ Q12/q12.js
@@ -0,0 +1,7 @@
+// Q12: 
+var names = ["Abdullah", "Ateeq Ahmed", "Huzaifa", "Abdulrab"];
+var message = "how are you?";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
---
+++ Q12/q12.ts
@@ -0,0 +1,11 @@
+// Q12: Q12Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names : string [] = ["Abdullah", "Ateeq Ahmed", "Huzaifa", "Abdulrab"];
+let message : string = "how are you?";
+
+console.log(names[0] + " " +message);
+console.log(names[1] + " " +message);
+console.log(names[2] + " " +message);
+console.log(names[3] + " " +message);
+
+
